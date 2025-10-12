ba91d3f3bab68eec800d4e036bbb3da7ec104306
João Job <joao.gabrielfjob@gmail.com>
2025-10-11 21:08:22 -0300

    fix: add --no-sandbox to browser launch

--- scrap/src/index.ts
+++ scrap/src/index.ts
@@ -1,7 +1,15 @@
 import puppeteer from 'puppeteer'
 import { matchHandler } from './handler/match_handler'
 
-const browser = await puppeteer.launch({ headless: true })
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu'
+  ]
+})
 
 Promise.all([
   matchHandler.scrapeFinishedMatches(browser),
